369a1635e59e3a9379b4bf269b22b027cf33bd4b
layse-garcia <63252079+layse-garcia@users.noreply.github.com>
2020-07-31 23:21:54 -0300

    deu ruim agora

---
+++ gymlife/Controle/TreinoDAO.php
@@ -0,0 +1,70 @@
+<?php
+	// Persistence
+	include_once('../Modelo/Treino.php');
+	class TreinoDAO{
+
+		public function __construct(){}
+		
+		public function addTreino($treino, $con){
+            $sql = "INSERT INTO Treino(Cliente_Pessoa_idPessoa, Funcionario_Pessoa_idPessoa, Equipamento_idEquipamento, Tipo_treino, serie, repeticoes, peso)
+                    VALUES('".$treino->getTreinoIdPessoa()."','".$treino->getTreinoIdFuncionario."','".$treino->getTreinoIdEquipamento."',
+                    '".$treino->getTipo()."','".$treino->getSerie()."','".$treino->getRepeticoes()."','".$treino->getPeso()."');";
+            
+            echo $sql;
+            $resultadoT = mysqli_query($con,$sql) or die(mysqli_error($con));
+            if($resultadoT == true){
+                echo 'Treino cadastrado.';
+            }else{
+                echo 'Algo ocorreu: ' . mysqli_error($con);
+            }
+        }
+        
+        public function atualizarTreino($idPessoaA, $idFuncionarioA, $idEquipamentoA, $tipoA, $serieA, $repeticoesA, $pesoA, $codigo, $con){
+			$sql = "UPDATE Treino SET Cliente_Pessoa_idPessoa = '".$idPessoaA."',
+									  Funcionario_Pessoa_idPessoa = '".$idFuncionarioA."',
+									  Equipamento_idEquipamento = '".$idEquipamentoA."',
+									  Tipo_treino = '".$tipoA."',
+									  serie = '".$serieA."',
+									  repeticoes = '".$repeticoesA."',
+									  peso = '".$pesoA."'
+					WHERE idTreino = '".$codigo."';
+			";
+			$resultadoT = mysqli_query($con,$sql) or die(mysqli_error($con));
+			// VERIFICA SE TUDO DEU CERTO
+			if ($resultadoT == true){
+				echo 'Treino alterado com sucesso';
+				/*echo '<SCRIPT type="text/javascript"> //not showing me this
+							alert("Treino alterado com sucesso!");
+							window.location.replace("menu.php");
+					</SCRIPT>';  */     
+			}else{
+				echo '<script>alert("Problema ao alterar Treino no banco de dados");</script>';
+				echo 'O erro que aconteceu foi este: ' . mysqli_error($con).'<br>';
+				echo '<a href="menu.php"> VOLTAR </a>';
+			}
+		}
+		
+		public function excluirTreino($codigo, $con){
+			//criar a string sql que exclui o usuario do banco de dados
+			$sql = "DELETE FROM Treino WHERE idTreino=".$codigo.";";
+
+			//executa o comando DELETE no banco de dados para o usuario que tem
+			//aquele codigo especifico
+			$resultadoT = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+			//avaliando o resultado
+			if ($resultadoT == true){
+				echo 'Excluído Treino';
+				/*echo '<SCRIPT type="text/javascript"> //not showing me this
+								alert("Treino excluído com sucesso");
+								window.location.replace("listarpessoas.php");
+						</SCRIPT>';*/
+			}else{
+				echo 'Problema ao apagar o registro no banco de dados <br>';
+				echo 'O erro que aconteceu foi este: ' . mysqli_error($con);
+				echo '<a href="menu.php"> MENU </a>';
+			}
+		}
+        
+	}
+?>
--- gymlife/Controle/teste.php
+++ gymlife/Controle/teste.php
@@ -6,6 +6,13 @@
 	include_once('../Controle/ClienteDAO.php');
 	include_once('../Modelo/Instrutor.php');
 	include_once('../Controle/InstrutorDAO.php');
+	include_once('../Modelo/Plano.php');
+	include_once('../Controle/PlanoDAO.php');
+	include_once('../Modelo/Equipamento.php');
+	include_once('../Controle/EquipamentoDAO.php');
+	include_once('../Modelo/Treino.php');
+	include_once('../Controle/TreinoDAO.php');
+	
 	
 	$cpfC = '321456';
 	$nomeC = 'FabioPaes';
@@ -17,7 +24,8 @@
 	$planoC = '2';
 	$salarioI = '900.00';
 	$cHI = '20';
-	$imagem = 'abcdef.jpeg'; 
+	$imagem = 'abcdef.jpeg';
+	$codigo = '1';
 	
 	// Nos arquivos que precisam de conectar com o BD
 	$conexao = new ConexaoBD();
@@ -27,23 +35,55 @@
 	$pessoaDAO = new PessoaDAO();
 	//$pessoaDAO->addPessoa($p,$conexao);
 	//$pessoaDAO->teste($p);
-	$codigo = '18';
+	
 	//$pessoaDAO->atualizarPessoa($cpfC, $nomeC, $telefoneC, $emailC, $dataNascC, $senhaC, $cargo, $codigo, $conexao);
+	//$pessoaDAO->excluirPessoa($codigo, $conexao);
 	
 	//$c = new Cliente($planoC);
 	//$c1 = new ClienteDAO();
 	//$c1->addCliente($c,$conexao, $p->getCPF());
-	
+	//$c1->atualizarCliente($planoC, $codigo, $conexao);
 	
 	//$i = new Instrutor($salarioI, $cHI, $imagem);
 	//$i1 = new InstrutorDAO();
 	//$i1->addInstrutor($i,$conexao, $p->getCPF());
-	
-	
-	//$c1->atualizarCliente($planoC, $codigo, $conexao);
 	//$i1->atualizarInstrutor($salarioI, $cHI, $imagem, $codigo, $conexao);
 	
-	$pessoaDAO->excluirPessoa($codigo, $conexao);
+	
+	$nomeP = 'SEMESTRAL';
+	$qtdP = '6';
+	$valorP = '79.99';
+	//$plan = new Plano($nomeP, $qtdP, $valorP);
+	//$planDAO = new PlanoDAO();
+	//$planDAO->addPlano($plan, $conexao);
+	//$planDAO->atualizarPlano($nomeP, $qtdP, $valorP, $codigo, $conexao);
+	//$planDAO->excluirPlano($codigo, $conexao);
+	
+	$nomeEquip = 'Supino Duplo';
+	$qtdEquip = '2';
+	$marcaEquip = 'Stronger';
+	$anoEquip = '2014';
+	
+	//$equip = new Equipamento($nomeEquip, $qtdEquip, $marcaEquip, $anoEquip);
+	//$equipDAO = new EquipamentoDAO();
+	//$equipDAO->addEquipamento($equip, $conexao);
+	//$equipDAO->atualizarEquipamento($nomeEquip, $qtdEquip, $marcaEquip, $anoEquip, $codigo, $conexao);
+	//$equipDAO->excluirEquipamento($codigo, $conexao);
+	
+	$idPes = '50';
+	$idFunc = '53';
+	$idEquip = '2';
+	$tipo = 'A';
+	$serie = '4';
+	$repeticoes = '20';
+	$peso = '5';
+	
+	$trein = new Treino($idPes, $idFunc, $idEquip, $tipo, $serie, $repeticoes, $peso);
+	$treinDAO = new TreinoDAO();
+	$treinDAO->addTreino($trein, $conexao);
+	//$treinDAO->atualizarTreino($idPes, $idFunc, $idEquip, $tipo, $serie, $repeticoes, $peso, $codigo, $conexao);
+	//$treinDAO->excluirTreino($codigo, $conexao);
+	
 	
 	/*
 	$cliente1 = new Cliente($cpfC, $nomeC, $telefoneC, $emailC, $dataNascC, $senhaC, $cargo, $plano);
